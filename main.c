bdc553c9759cb62212d1585b09b0ab2d3907cd3c
breurlucas <lucasbreur@gmail.com>
2020-12-09 18:54:27 -0300

    Implemented bitmap structure reading and available storage calculation

--- main.c
+++ main.c
@@ -15,23 +15,76 @@ Montagem em Linux
 */
 
 /*
-O processo de esteganografia em imagens envolve a utilização de dois arquivos: uma imagem qualquer, 
-que carregará a informação escondida e é chamada de imagem de cobertura, e um outro arquivo, 
-que será a mensagem a ser escondida e pode ser outra imagem, um arquivo de texto ou qualquer 
-outro tipo de arquivo que possa ser escondido em uma imagem. A combinação destes dois arquivos 
-é chamada de estego-imagem.
+BITMAP file format
+
+Header: 54 bytes (below)
+Image Data: file size - 54
+
+typedef struct {              // Total: 54 bytes
+  uint16_t  type;             // Magic identifier: 0x4d42
+  uint32_t  size;             // File size in bytes
+  uint16_t  reserved1;        // Not used
+  uint16_t  reserved2;        // Not used
+  uint32_t  offset;           // Offset to image data in bytes from beginning of file (54 bytes)
+  uint32_t  dib_header_size;  // DIB Header size in bytes (40 bytes)
+  int32_t   width_px;         // Width of the image
+  int32_t   height_px;        // Height of image
+  uint16_t  num_planes;       // Number of color planes
+  uint16_t  bits_per_pixel;   // Bits per pixel
+  uint32_t  compression;      // Compression type
+  uint32_t  image_size_bytes; // Image size in bytes
+  int32_t   x_resolution_ppm; // Pixels per meter
+  int32_t   y_resolution_ppm; // Pixels per meter
+  uint32_t  num_colors;       // Number of colors  
+  uint32_t  important_colors; // Important colors 
+} BMPHeader;
+
+Source: https://engineering.purdue.edu/ece264/17au/hw/HW15
 */
+
+#define HEADER_SIZE 54
+#define SIZE_OFFSET 2
+#define BITS_PER_PIXEL_OFFSET 28
 
 int main() {
 
-    FILE *bmp, *file2;
-    int width, height;
+    FILE *file1, *file2;
+    file1 = fopen("test.bmp", "rb"); // rb, read binary mode
+    file2 = fopen("test.txt", "r");
+    unsigned int storage;
 
-    // 1. Check the available storage space in the bitmap
+    typedef struct {
+        unsigned int img_size;
+        unsigned int bits_per_pixel;
+        unsigned char* data;
+    } BMP;
 
-    // 2. Check the size of the file to be written onto the bitmap
+    /* 
+    1. Check the available storage space in the bitmap 
+    
+    fread:
+        - args: ptr destination, size in bytes, nr of elements of said size, input)
+    fseek: sets file position to a given offset in bytes
+        - args: input, offset, whence (the position from which the offset is applied, SEEK_SET, SEEK_CUR or SEEK_END) 
+    */
 
-    // 3. Check if there is enough space. If there isn't, prompt the user that there is no space available
+    BMP bmp;
 
-    // 4. Otherwise, insert data in the least signicant bit (LSB) of each byte
+    fseek(file1, SIZE_OFFSET, SEEK_SET);
+    fread(&bmp.img_size, 4, 1, file1);
+    bmp.img_size = bmp.img_size - HEADER_SIZE;
+
+    storage = bmp.img_size / 8 // This is the available storage space in bytes
+
+    /* 
+    2. Check the size of the file to be written onto the bitmap
+    */
+
+    /*
+    3. Check if there is enough space. If there isn't, prompt the user that there is no space available
+    */
+
+    /* 
+    4. Otherwise, insert data in the least signicant bit (LSB) of each byte
+    */
 }
