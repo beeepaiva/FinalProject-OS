584d535059e09627dee7b4eab389b8401f932300
Beatriz Paiva <beeepaiva@gmail.com>
2020-12-09 23:12:20 -0300

    Adding fuse

---
+++ CMakeFiles/fuse.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fuse.dir/fuse.c.o"
+  "bin/fuse"
+  "bin/fuse.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fuse.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
